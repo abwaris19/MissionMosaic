dd3825b83408966eaf46dcd471e4d1afb469d0b0
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-12 07:42:05 +0100

    Initial Commit

---
+++ MissionMosaic/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MissionMosaic
+//
+//  Created by Khan on 12.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MissionMosaic/MissionMosaicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissionMosaicApp.swift
+//  MissionMosaic
+//
+//  Created by Khan on 12.12.23.
+//
+
+import SwiftUI
+
+@main
+struct MissionMosaicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
