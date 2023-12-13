c7dbf510a9a10556afb05e7e8743039281a05caf
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-13 07:35:44 +0100

    implemented ViewModel into ListView

--- MissionMosaic/Views/ListView.swift
+++ MissionMosaic/Views/ListView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct ListView: View {
-    @State var items: [String] = [ "This is the first title", "This is the second title", "Third"]
+    @State var items: [ItemModel] = [
+    
+        ItemModel(title: "This is the First Title", isCompleted: false),
+        ItemModel(title: "This is the Second Title", isCompleted: true),
+        ItemModel(title: "Third", isCompleted: false)
+    ]
     var body: some View {
      
         List {
            
-            ForEach(items, id: \.self) { item in
-                ListRowView(title: item)
+            ForEach(items) { item in
+                ListRowView(item: item)
             }
             
         }
