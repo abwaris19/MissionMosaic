bb23af0c727e0e272fd372cfd65878c3c80ceacc
abdulwariskhan <hellokhan.ch@gmail.com>
2023-12-14 07:17:28 +0100

    created EnvironmentObject

--- MissionMosaic/MissionMosaicApp.swift
+++ MissionMosaic/MissionMosaicApp.swift
@@ -19,12 +19,16 @@ import SwiftUI
 
 @main
 struct MissionMosaicApp: App {
+    
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
     var body: some Scene {
         WindowGroup {
             
             NavigationView {
                 ListView() 
             }
+            .environmentObject(listViewModel)
            
         }
     }
